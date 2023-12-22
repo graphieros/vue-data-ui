bfc36ab025ccb332eedf2d6d6a8bc52bff43d4f0
graphieros <alec.lloyd.probert@gmail.com>
2023-12-22 16:52:07 +0100

    remove unused file

--- src/setups.js
+++
@@ -1,9 +0,0 @@
-export function setupTemplate(template, args = {}) {
-    const { args: moreArgs = {}} = template;
-    const templateClone = template.bind();
-    templateClone.args = {
-        ...moreArgs,
-        ...args
-    }
-    return templateClone;
-}
