762355ac3a787bcb02cfdb11e22ca3222f2d5a64
graphieros <alec.lloyd.probert@gmail.com>
2024-05-07 14:17:45 +0200

    VueUiNestedDonuts reduced blur filter

--- src/components/vue-ui-nested-donuts.vue
+++ src/components/vue-ui-nested-donuts.vue
@@ -689,7 +689,7 @@ defineExpose({
 
             <defs>
                 <filter :id="`blur_${uid}`" x="-50%" y="-50%" width="200%" height="200%">
-                    <feGaussianBlur in="SourceGraphic" :stdDeviation="5" :id="`blur_std_${uid}`" />
+                    <feGaussianBlur in="SourceGraphic" :stdDeviation="2" :id="`blur_std_${uid}`" />
                     <feColorMatrix type="saturate" values="0" />
                 </filter>
             </defs>
