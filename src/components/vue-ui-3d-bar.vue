818a6602fa9bb371e3b4ea46546ce65e44b7774e
graphieros <alec.lloyd.probert@gmail.com>
2024-06-02 10:03:14 +0200

    Improvement - VueUi3dBar - Added close button on table

--- src/components/vue-ui-3d-bar.vue
+++ src/components/vue-ui-3d-bar.vue
@@ -871,6 +871,7 @@ defineExpose({
             :body="dataTable.body"
             :config="dataTable.config"
             :title="`${barConfig.style.chart.title.text}${barConfig.style.chart.title.subtitle.text ? ` : ${barConfig.style.chart.title.subtitle.text}` : ''}`"
+            @close="mutableConfig.showTable = false"
         >
             <template #th="{ th }">
                 <div v-html="th" style="display:flex;align-items:center"></div>
