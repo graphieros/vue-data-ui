a0639ffc436c4fb63088f5c207e5b34d56f6e9e8
graphieros <alec.lloyd.probert@gmail.com>
2024-06-02 09:57:32 +0200

    Improvement - VueUiRings - Added close button on table

--- src/components/vue-ui-rings.vue
+++ src/components/vue-ui-rings.vue
@@ -617,6 +617,7 @@ defineExpose({
             :body="dataTable.body"
             :config="dataTable.config"
             :title="`${ringsConfig.style.chart.title.text}${ringsConfig.style.chart.title.subtitle.text ? ` : ${ringsConfig.style.chart.title.subtitle.text}` : ''}`"
+            @close="mutableConfig.showTable = false"
         >
           <template #th="{th}">
               <div v-html="th" style="display:flex;align-items:center"></div>
