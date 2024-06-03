3f38eab9b1b54129d7ba2f0f0fe3091c82322f87
graphieros <alec.lloyd.probert@gmail.com>
2024-06-03 07:35:27 +0200

    Improvement - VueUiStripPlot - Added data table inside an accordion for smoother open transition

--- src/components/vue-ui-strip-plot.vue
+++ src/components/vue-ui-strip-plot.vue
@@ -27,6 +27,7 @@ import Tooltip from "../atoms/Tooltip.vue";
 import DataTable from "../atoms/DataTable.vue";
 import Skeleton from "./vue-ui-skeleton.vue";
 import Shape from "../atoms/Shape.vue";
+import Accordion from "./vue-ui-accordion.vue";
 
 const props = defineProps({
     config: {
@@ -647,22 +648,28 @@ defineExpose({
             </template>
         </Tooltip>
 
-        <DataTable
-            v-if="mutableConfig.showTable && isDataset"
-            :colNames="dataTable.colNames"
-            :head="dataTable.head" 
-            :body="dataTable.body"
-            :config="dataTable.config"
-            :title="`${stripConfig.style.chart.title.text}${stripConfig.style.chart.title.subtitle.text ? ` : ${stripConfig.style.chart.title.subtitle.text}` : ''}`"
-            @close="mutableConfig.showTable = false"
-        >
-            <template #th="{ th }">
-                <div v-html="th" style="display:flex;align-items:center"></div>
+        <Accordion hideDetails v-if="isDataset" :config="{
+            open: mutableConfig.showTable,
+            maxHeight: 10000
+        }">
+            <template #content>
+                <DataTable
+                    :colNames="dataTable.colNames"
+                    :head="dataTable.head" 
+                    :body="dataTable.body"
+                    :config="dataTable.config"
+                    :title="`${stripConfig.style.chart.title.text}${stripConfig.style.chart.title.subtitle.text ? ` : ${stripConfig.style.chart.title.subtitle.text}` : ''}`"
+                    @close="mutableConfig.showTable = false"
+                >
+                    <template #th="{ th }">
+                        <div v-html="th" style="display:flex;align-items:center"></div>
+                    </template>
+                    <template #td="{ td }">
+                        {{ td.name || td }}
+                    </template>
+                </DataTable>
             </template>
-            <template #td="{ td }">
-                {{ td.name || td }}
-            </template>
-        </DataTable>
+        </Accordion>
     </div>
 </template>
 
