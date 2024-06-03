c29227c12089744ddae3b7247b1d3468fb090cba
graphieros <alec.lloyd.probert@gmail.com>
2024-06-03 07:35:46 +0200

    Improvement - VueUiTreemap - Added data table inside an accordion for smoother open transition

--- src/components/vue-ui-treemap.vue
+++ src/components/vue-ui-treemap.vue
@@ -10,6 +10,7 @@ import Tooltip from "../atoms/Tooltip.vue";
 import Legend from "../atoms/Legend.vue";
 import Skeleton from "./vue-ui-skeleton.vue";
 import DataTable from '../atoms/DataTable.vue';
+import Accordion from './vue-ui-accordion.vue';
 import {
     adaptColorToBackground,
     convertColorToHex,
@@ -623,22 +624,28 @@ defineExpose({
         </Tooltip>
 
         <!-- DATA TABLE -->
-        <DataTable
-            v-if="mutableConfig.showTable && isDataset"
-            :colNames="dataTable.colNames"
-            :head="dataTable.head" 
-            :body="dataTable.body"
-            :config="dataTable.config"
-            :title="`${treemapConfig.style.chart.title.text}${treemapConfig.style.chart.title.subtitle.text ? ` : ${treemapConfig.style.chart.title.subtitle.text}` : ''}`"
-            @close="mutableConfig.showTable = false"
-        >
-            <template #th="{ th }">
-                <div v-html="th" style="display:flex;align-items:center"></div>
+        <Accordion hideDetails v-if="isDataset" :config="{
+            open: mutableConfig.showTable,
+            maxHeight: 10000
+        }">
+            <template #content>
+                <DataTable
+                    :colNames="dataTable.colNames"
+                    :head="dataTable.head" 
+                    :body="dataTable.body"
+                    :config="dataTable.config"
+                    :title="`${treemapConfig.style.chart.title.text}${treemapConfig.style.chart.title.subtitle.text ? ` : ${treemapConfig.style.chart.title.subtitle.text}` : ''}`"
+                    @close="mutableConfig.showTable = false"
+                >
+                    <template #th="{ th }">
+                        <div v-html="th" style="display:flex;align-items:center"></div>
+                    </template>
+                    <template #td="{ td }">
+                        {{ td.name || td }}
+                    </template>
+                </DataTable>
             </template>
-            <template #td="{ td }">
-                {{ td.name || td }}
-            </template>
-        </DataTable>
+        </Accordion>
     </div>
 </template>
 
