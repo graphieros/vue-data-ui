5e9d687ac848a44286b451455c231bc8eb66c9d1
graphieros <alec.lloyd.probert@gmail.com>
2024-06-06 13:36:34 +0200

    New feature - VueUiScatter - Implemented themes

--- src/components/vue-ui-scatter.vue
+++ src/components/vue-ui-scatter.vue
@@ -13,6 +13,7 @@ import {
     opacity, 
     palette,
     createSmoothPath,
+    themePalettes,
     XMLNS,
 dataLabel,
 convertCustomPalette
@@ -20,6 +21,7 @@ convertCustomPalette
 import pdf from "../pdf";
 import img from "../img";
 import mainConfig from "../default_configs.json";
+import themes from "../themes.json";
 import { useNestedProp } from "../useNestedProp";
 import Title from "../atoms/Title.vue";
 import UserOptions from "../atoms/UserOptions.vue";
@@ -80,10 +82,21 @@ onMounted(() => {
 });
 
 const scatterConfig = computed(() => {
-    return useNestedProp({
+    const mergedConfig = useNestedProp({
         userConfig: props.config,
         defaultConfig: defaultConfig.value
     });
+    if (mergedConfig.theme) {
+        return {
+            ...useNestedProp({
+                userConfig: themes.vue_ui_scatter[mergedConfig.theme] || props.config,
+                defaultConfig: mergedConfig
+            }),
+            customPalette: themePalettes[mergedConfig.theme] || palette
+        }
+    } else {
+        return mergedConfig;
+    }
 });
 
 const customPalette = computed(() => {
