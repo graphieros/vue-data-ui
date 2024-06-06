e03482bca48420ad2b35f331d98bf38910667b38
graphieros <alec.lloyd.probert@gmail.com>
2024-06-06 13:38:22 +0200

    New feature - VueUiSparkStackbar - Implemented themes

--- src/components/vue-ui-sparkstackbar.vue
+++ src/components/vue-ui-sparkstackbar.vue
@@ -11,9 +11,11 @@ import {
     opacity, 
     palette, 
     shiftHue,
+    themePalettes,
     XMLNS 
 } from "../lib";
 import mainConfig from "../default_configs.json";
+import themes from "../themes.json";
 import { useNestedProp } from "../useNestedProp";
 import Skeleton from "./vue-ui-skeleton.vue";
 
@@ -40,10 +42,21 @@ const uid = ref(createUid());
 const defaultConfig = ref(mainConfig.vue_ui_sparkstackbar);
 
 const stackConfig = computed(() => {
-    return useNestedProp({
+    const mergedConfig = useNestedProp({
         userConfig: props.config,
         defaultConfig: defaultConfig.value
     });
+    if (mergedConfig.theme) {
+        return {
+            ...useNestedProp({
+                userConfig: themes.vue_ui_sparkstackbar[mergedConfig.theme] || props.config,
+                defaultConfig: mergedConfig
+            }),
+            customPalette: themePalettes[mergedConfig.theme] || palette
+        }
+    } else {
+        return mergedConfig;
+    }
 });
 
 const customPalette = computed(() => {
