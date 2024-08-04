087f703ac04bdf5a6caa28c8d3c0a8524340a27f
graphieros <alec.lloyd.probert@gmail.com>
2024-08-04 11:28:06 +0200

    Improvement - VueUiRadar - Exposed toggleTable method

--- src/components/vue-ui-radar.vue
+++ src/components/vue-ui-radar.vue
@@ -500,11 +500,16 @@ function toggleFullscreen(state) {
     step.value += 1;
 }
 
+function toggleTable() {
+    mutableConfig.value.showTable = !mutableConfig.value.showTable;
+}
+
 defineExpose({
     getData,
     generatePdf,
     generateCsv,
-    generateImage
+    generateImage,
+    toggleTable
 });
 
 </script>
@@ -557,7 +562,7 @@ defineExpose({
             @generatePdf="generatePdf"
             @generateCsv="generateCsv"
             @generateImage="generateImage"
-            @toggleTable="mutableConfig.showTable = !mutableConfig.showTable"
+            @toggleTable="toggleTable"
         />
 
         <!-- CHART -->
