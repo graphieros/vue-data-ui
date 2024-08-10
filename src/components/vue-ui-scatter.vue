ad61445620bc82b72ad3921275bed1af10b606d4
graphieros <alec.lloyd.probert@gmail.com>
2024-08-10 08:32:47 +0200

    Improvement - VueUiScatter - Implemented slots for user options button contents

--- src/components/vue-ui-scatter.vue
+++ src/components/vue-ui-scatter.vue
@@ -576,9 +576,11 @@ defineExpose({
             :isImaging="isImaging"
             :isPrinting="isPrinting"
             :uid="uid"
-            :hasImg="true"
-            hasTable
-            hasFullscreen
+            :hasPdf="scatterConfig.userOptions.buttons.pdf"
+            :hasImg="scatterConfig.userOptions.buttons.img"
+            :hasXls="scatterConfig.userOptions.buttons.csv"
+            :hasTable="scatterConfig.userOptions.buttons.table"
+            :hasFullscreen="scatterConfig.userOptions.buttons.fullscreen"
             :isFullscreen="isFullscreen"
             :chartElement="scatterChart"
             @toggleFullscreen="toggleFullscreen"
@@ -586,7 +588,23 @@ defineExpose({
             @generateCsv="generateCsv"
             @generateImage="generateImage"
             @toggleTable="toggleTable"
-        />
+        >
+        <template #pdf v-if="$slots.pdf">
+                <slot name="pdf" />
+            </template>
+            <template #csv v-if="$slots.csv">
+                <slot name="csv" />
+            </template>
+            <template #img v-if="$slots.img">
+                <slot name="img" />
+            </template>
+            <template #table v-if="$slots.table">
+                <slot name="table" />
+            </template>
+            <template v-if="$slots.fullscreen" #fullscreen="{ toggleFullscreen, isFullscreen }">
+                <slot name="fullscreen" v-bind="{ toggleFullscreen, isFullscreen }"/>
+            </template>
+        </UserOptions>
 
         <!-- CHART -->
         <svg :xmlns="XMLNS" v-if="isDataset" :class="{ 'vue-data-ui-fullscreen--on': isFullscreen, 'vue-data-ui-fulscreen--off': !isFullscreen }" :viewBox="`0 0 ${svg.width} ${svg.height}`" :style="`max-width:100%;overflow:visible;background:${scatterConfig.style.backgroundColor};color:${scatterConfig.style.color}`">
