e8cc0f919acfa9b247649699d2ec80e52b2b4600
graphieros <alec.lloyd.probert@gmail.com>
2024-09-25 09:44:16 +0200

    Improvement - VueUiTreemap - Added backgroundOpacity tooltip config option

--- src/components/vue-ui-treemap.vue
+++ src/components/vue-ui-treemap.vue
@@ -661,6 +661,7 @@ defineExpose({
             :borderRadius="FINAL_CONFIG.style.chart.tooltip.borderRadius"
             :borderColor="FINAL_CONFIG.style.chart.tooltip.borderColor"
             :borderWidth="FINAL_CONFIG.style.chart.tooltip.borderWidth"
+            :backgroundOpacity="FINAL_CONFIG.style.chart.tooltip.backgroundOpacity"
             :parent="treemapChart"
             :content="tooltipContent"
             :isCustom="isFunction(FINAL_CONFIG.style.chart.tooltip.customFormat)"
