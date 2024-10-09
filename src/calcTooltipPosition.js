b8fa755daf9ce4af3e9b13a7e736edbdb37df5c2
graphieros <alec.lloyd.probert@gmail.com>
2024-10-09 17:34:22 +0200

    Tooltip - Add position features to tooltip

--- src/calcTooltipPosition.js
+++ src/calcTooltipPosition.js
@@ -1,22 +1,40 @@
 import { ref } from "vue";
 
-export function calcTooltipPosition({ tooltip, chart, clientPosition}) {
+export function calcTooltipPosition({ tooltip, chart, clientPosition, positionPreference = 'center', defaultOffsetY = 24}) {
     const offsetX = ref(0);
-    const offsetY = ref(48);
+    const offsetY = ref(defaultOffsetY);
     if (tooltip && chart) {
         const { width, height } = tooltip.getBoundingClientRect();
         const { right, left, bottom } = chart.getBoundingClientRect();
 
-        if (clientPosition.x + width / 2 > right) {
-            offsetX.value = -width;
-        } else if (clientPosition.x - width / 2 < left) {
-            offsetX.value = 0;
-        } else {
-            offsetX.value = -width / 2;
+        if (positionPreference === 'center') {
+            if (clientPosition.x + width / 2 > right) {
+                offsetX.value = -width + (right - clientPosition.x)
+            } else if (clientPosition.x - width / 2 < left) {
+                offsetX.value = -width + (width - (clientPosition.x - left))
+            } else {
+                offsetX.value = -width / 2;
+            }
+        }
+
+        if (positionPreference === 'right') {
+            if (clientPosition.x + width > right) {
+                offsetX.value = -width + (right - clientPosition.x)
+            } else {
+                offsetX.value = 0;
+            }
+        }
+
+        if (positionPreference === 'left') {
+            if (clientPosition.x < left + width) {
+                offsetX.value = -width + (width - (clientPosition.x - left))
+            } else {
+                offsetX.value = -width;
+            }
         }
 
         if (clientPosition.y + height > bottom) {
-            offsetY.value = -height - 48;
+            offsetY.value = -height - defaultOffsetY;
         }
     }
     return {
