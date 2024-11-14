2bf4418cbf87522287996bc701d734fa339df129
graphieros <alec.lloyd.probert@gmail.com>
2024-11-14 16:22:28 +0100

    Implement dynamic import of pdf and img deps

--- src/usePrinter.js
+++ src/usePrinter.js
@@ -1,46 +1,61 @@
 import { ref } from "vue";
-import pdf from "./pdf";
-import img from "./img";
 
 export function usePrinter({
     elementId,
-    fileName
+    fileName,
+    canPrint = true
 }) {
     const isPrinting = ref(false);
     const isImaging = ref(false);
     const __to__ = ref(null);
 
-    function generatePdf(){
+    async function generatePdf() {
+        if (!canPrint || isPrinting.value) return;
         isPrinting.value = true;
         clearTimeout(__to__.value);
-        __to__.value = setTimeout(() => {
-            pdf({
-                domElement: document.getElementById(elementId),
-                fileName,
-            }).finally(() => {
-                isPrinting.value = false;
-            });
-        }, 100)
+        __to__.value = setTimeout(async () => {
+            if (canPrint) {
+                try {
+                    const { default: pdf } = await import("./pdf");
+                    await pdf({
+                        domElement: document.getElementById(elementId),
+                        fileName,
+                    });
+                } catch (error) {
+                    console.error("Error generating PDF:", error);
+                } finally {
+                    isPrinting.value = false;
+                }
+            }
+        }, 100);
     }
 
-    function generateImage() {
+    async function generateImage() {
+        if (!canPrint || isImaging.value) return;
         isImaging.value = true;
         clearTimeout(__to__.value);
-        __to__.value = setTimeout(() => {
-            img({
-                domElement: document.getElementById(elementId),
-                fileName,
-                format: 'png'
-            }).finally(() => {
-                isImaging.value = false;
-            }, 100)
-        })
+        __to__.value = setTimeout(async () => {
+            if (canPrint) {
+                try {
+                    const { default: img } = await import("./img");
+                    await img({
+                        domElement: document.getElementById(elementId),
+                        fileName,
+                        format: "png",
+                    });
+                } catch (error) {
+                    console.error("Error generating image:", error);
+                } finally {
+                    isImaging.value = false;
+                }
+            }
+        }, 100);
     }
 
     return {
         generatePdf,
         generateImage,
         isPrinting,
-        isImaging
-    }
+        isImaging,
+    };
 }
