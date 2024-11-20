75f4254f31af7ab95107721fe0cba2de2ec31dda
graphieros <alec.lloyd.probert@gmail.com>
2024-11-20 08:11:37 +0100

    Types - Update VueUiRatingConfig & VueUiSmileyConfig types

--- types/vue-data-ui.d.ts
+++ types/vue-data-ui.d.ts
@@ -3973,6 +3973,7 @@ declare module 'vue-data-ui' {
                 position?: "bottom" | "top" | "left" | "right",
                 offsetY?: number;
                 offsetX?: number;
+                formatter?: Formatter;
             };
             tooltip?: ChartTooltip & {
                 offsetY?: number;
@@ -4026,10 +4027,13 @@ declare module 'vue-data-ui' {
                 position?: string;
                 offsetY?: number;
                 offsetX?: number;
+                formatter?: Formatter;
             };
             tooltip?: ChartTooltip & {
                 offsetY?: number;
                 bold?: boolean;
+                roundingValue?: number;
+                formatter?: Formatter;
             };
         };
     };
