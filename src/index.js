1dd5696c8048150ce16e2ba2afca2c278b44b8d3
graphieros <alec.lloyd.probert@gmail.com>
2024-12-10 17:35:08 +0100

    Build - Add VueUiHistoryPlot export

--- src/index.js
+++ src/index.js
@@ -64,6 +64,7 @@ export const VueUiGizmo = defineAsyncComponent(() => import('./components/vue-ui
 export const VueUiStackbar = defineAsyncComponent(() => import('./components/vue-ui-stackbar.vue'))
 export const VueUiBullet = defineAsyncComponent(() => import('./components/vue-ui-bullet.vue'))
 export const VueUiFunnel = defineAsyncComponent(() => import('./components/vue-ui-funnel.vue'))
+export const VueUiHistoryPlot = defineAsyncComponent(() => import('./components/vue-ui-history-plot.vue'))
 export {
     createWordCloudDatasetFromPlainText,
     getPalette,
