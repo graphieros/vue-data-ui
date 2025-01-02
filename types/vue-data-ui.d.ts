0b5cc86334a94ad825db229e45fc51a338a7e991
graphieros <alec.lloyd.probert@gmail.com>
2025-01-02 17:57:08 +0100

    Types - Update ChartZoom type

--- types/vue-data-ui.d.ts
+++ types/vue-data-ui.d.ts
@@ -121,6 +121,8 @@ declare module 'vue-data-ui' {
         minimap?: ZoomMinimap;
         startIndex?: number | null;
         endIndex?: number | null;
+        enableRangeHandles?: boolean;
+        enableSelectionDrag?: boolean;
     }
 
     export type Theme = "" | "zen" | "hack" | "concrete";
@@ -4501,6 +4503,8 @@ declare module 'vue-data-ui' {
         zoomMinimap?: ZoomMinimap;
         zoomStartIndex?: number | null;
         zoomEndIndex?: number | null;
+        zoomEnableRangeHandles?: boolean;
+        zoomEnableSelectionDrag?: boolean;
         userOptionsPosition?: 'right' | 'left';
         userOptionsButtons?: {
             tooltip?: boolean;
