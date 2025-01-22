bfe651974d18942832b5749c77c0250fec3a2b54
graphieros <alec.lloyd.probert@gmail.com>
2025-01-22 17:41:46 +0100

    Dev environment - Update testing arena

--- src/App.vue
+++ src/App.vue
@@ -56,57 +56,57 @@ import ArenaVueUiHistoryPlot from "../TestingArena/ArenaVueUiHistoryPlot.vue";
 const showOldArena = ref(false);
 
 const components = ref([             //--- CELEBRATION THEMES
-  /*____________________*/"VueUiXy", //  0 OK
-  /*_________________*/"VueUiDonut", //  1 OK
-  /*_______________*/"VueUiTreemap", //  2 OK
-  /*________________*/"VueUiWaffle", //  3 OK
-  /*_________________*/"VueUiRadar", //  4 OK
-  /*______________*/"VueUiQuadrant", //  5 OK
-  /*_________________*/"VueUiGauge", //  6 OK
-  /*_________________*/"VueUiWheel", //  7 OK
-  /*_____________*/"VueUiTiremarks", //  8 OK
-  /*______________*/"VueUiChestnut", //  9 OK
-  /*_________________*/"VueUiOnion", // 10 OK
-  /*___________*/"VueUiVerticalBar", // 11 OK
-  /*_______________*/"VueUiHeatmap", // 12 OK
-  /*_______________*/"VueUiScatter", // 13 OK
-  /*___________*/"VueUiCandlestick", // 14 OK
-  /*_____________*/"VueUiSparkline", // 15 OK
-  /*______________*/"VueUiSparkbar", // 16 OK
-  /*_________*/"VueUiSparkStackbar", // 17 OK
-  /*________*/"VueUiSparkHistogram", // 18 OK
-  /*____________*/"VueUiSparkgauge", // 19 OK
-  /*____________*/"VueUiSparkTrend", // 20 OK
-  /*____________*/"VueUiQuickChart", // 21 OK
-  /*____________*/"VueUiAgePyramid", // 22 OK
-  /*________*/"VueUiRelationCircle", // 23 OK
-  /*___________*/"VueUiThermometer", // 24 OK
-  /*_________________*/"VueUiRings", // 25 OK
-  /*________*/"VueUiDonutEvolution", // 26 OK
-  /*__________________*/"VueUiIcon", // 27 _/
-  /*_____________*/"VueUiMoodRadar", // 28 OK
-  /*______________*/"VueUiMolecule", // 29 OK
-  /*__________*/"VueUiNestedDonuts", // 30 OK
-  /*________________*/"VueUiGalaxy", // 31 OK
-  /*_____________*/"VueUiStripPlot", // 32 OK
-  /*______________*/"VueUiDumbbell", // 33 OK
-  /*_________________*/"VueUi3dBar", // 34 OK
-  /*________*/"VueUiTableSparkline", // 35 OK
-  /*__________*/"VueUiTableHeatmap", // 36 OK
-  /*____________ */"VueUiWordCloud", // 37 OK
-  /*______________*/"VueUiXyCanvas", // 38 OK
-  /*__________________*/"VueUiFlow", // 39 OK
-  /**/"VueUiParallelCoordinatePlot", // 40 OK
-  /*_________________*/"VueUiTimer", // 41 _/
-  /*_________*/"VueUiCarouselTable", // 42 _/
-  /*_________________*/"VueUiGizmo", // 43 _/
-  /*___________________*/"VueUiKpi", // 44 _/
-  /*______________*/"VueUiStackbar", // 45 OK
-  /*________________*/"VueUiRating", // 46 _/
-  /*________________*/"VueUiSmiley", // 47 _/
-  /*________________*/"VueUiBullet", // 48 OK
-  /*________________*/"VueUiFunnel", // 49 OK
-  /*___________*/"VueUiHistoryPlot", // 50 OK
+  /*____________________*/"VueUiXy", //  0 OK 
+  /*_________________*/"VueUiDonut", //  1 OK -
+  /*_______________*/"VueUiTreemap", //  2 OK -
+  /*________________*/"VueUiWaffle", //  3 OK -
+  /*_________________*/"VueUiRadar", //  4 OK -
+  /*______________*/"VueUiQuadrant", //  5 OK -
+  /*_________________*/"VueUiGauge", //  6 OK /
+  /*_________________*/"VueUiWheel", //  7 OK /
+  /*_____________*/"VueUiTiremarks", //  8 OK /
+  /*______________*/"VueUiChestnut", //  9 OK -
+  /*_________________*/"VueUiOnion", // 10 OK -
+  /*___________*/"VueUiVerticalBar", // 11 OK -
+  /*_______________*/"VueUiHeatmap", // 12 OK -
+  /*_______________*/"VueUiScatter", // 13 OK -
+  /*___________*/"VueUiCandlestick", // 14 OK -
+  /*_____________*/"VueUiSparkline", // 15 OK /
+  /*______________*/"VueUiSparkbar", // 16 OK /
+  /*_________*/"VueUiSparkStackbar", // 17 OK /
+  /*________*/"VueUiSparkHistogram", // 18 OK /
+  /*____________*/"VueUiSparkgauge", // 19 OK /
+  /*____________*/"VueUiSparkTrend", // 20 OK /
+  /*____________*/"VueUiQuickChart", // 21 OK -
+  /*____________*/"VueUiAgePyramid", // 22 OK -
+  /*________*/"VueUiRelationCircle", // 23 OK /
+  /*___________*/"VueUiThermometer", // 24 OK /
+  /*_________________*/"VueUiRings", // 25 OK -
+  /*________*/"VueUiDonutEvolution", // 26 OK -
+  /*__________________*/"VueUiIcon", // 27 _/ -
+  /*_____________*/"VueUiMoodRadar", // 28 OK -
+  /*______________*/"VueUiMolecule", // 29 OK -
+  /*__________*/"VueUiNestedDonuts", // 30 OK -
+  /*________________*/"VueUiGalaxy", // 31 OK -
+  /*_____________*/"VueUiStripPlot", // 32 OK -
+  /*______________*/"VueUiDumbbell", // 33 OK -
+  /*_________________*/"VueUi3dBar", // 34 OK -
+  /*________*/"VueUiTableSparkline", // 35 OK /
+  /*__________*/"VueUiTableHeatmap", // 36 OK -
+  /*____________ */"VueUiWordCloud", // 37 OK -
+  /*______________*/"VueUiXyCanvas", // 38 OK -
+  /*__________________*/"VueUiFlow", // 39 OK -
+  /**/"VueUiParallelCoordinatePlot", // 40 OK -
+  /*_________________*/"VueUiTimer", // 41 _/ /
+  /*_________*/"VueUiCarouselTable", // 42 _/ /
+  /*_________________*/"VueUiGizmo", // 43 _/ /
+  /*___________________*/"VueUiKpi", // 44 _/ /
+  /*______________*/"VueUiStackbar", // 45 OK /
+  /*________________*/"VueUiRating", // 46 _/ /
+  /*________________*/"VueUiSmiley", // 47 _/ /
+  /*________________*/"VueUiBullet", // 48 OK /
+  /*________________*/"VueUiFunnel", // 49 OK /
+  /*___________*/"VueUiHistoryPlot", // 50 OK -
 
   // screenshot
   // dashboard
