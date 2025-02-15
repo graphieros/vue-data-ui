4da672508e876d7c304bf771ba0e74da422294c0
graphieros <alec.lloyd.probert@gmail.com>
2025-02-15 08:09:39 +0100

    Tests - Improve UserOptions component tests

--- src/atoms/UserOptions.cy.js
+++ src/atoms/UserOptions.cy.js
@@ -2,7 +2,7 @@ import UserOptions from './UserOptions.vue'
 
 describe('<UserOptions />', () => {
 
-  beforeEach(function () {
+  function mount() {
     cy.mount(UserOptions, {
       props: {
         hasTooltip: true,
@@ -10,26 +10,39 @@ describe('<UserOptions />', () => {
         hasTable: true,
         hasSort: true,
         hasLabel: true,
-        hasStack: true
+        hasStack: true,
+        hasSort: true,
+        hasFullscreen: true,
+        hasAnimation: true,
+        hasAnnotator: true
       }
     });
+  }
+
+  function open() {
     cy.get(`[data-cy="user-options-summary"]`).click()
     cy.get(`[data-cy="user-options-drawer"]`).should('exist').and('be.visible')
-  })
+  }
   
   it('opens and closes on menu click', () => {
-    cy.get(`[data-cy="user-options-summary"]`).click()
-    cy.wait(200 )
-    cy.get(`[data-cy="user-options-drawer"]`).should('not.be.visible')
+    mount();
+    open();
+    cy.get(`[data-cy="user-options-summary"]`).click();
+    cy.wait(200 );
+    cy.get(`[data-cy="user-options-drawer"]`).should('not.be.visible');
   })
 
   it('closes the drawer on click outside', () => {
-    cy.get('body').click(0,0, { force: true})
-    cy.wait(200 )
-    cy.get(`[data-cy="user-options-drawer"]`).should('not.be.visible')
+    mount();
+    open();
+    cy.get('body').click(0,0, { force: true});
+    cy.wait(200);
+    cy.get(`[data-cy="user-options-drawer"]`).should('not.be.visible');
   })
 
   it('contains all necessary icons', () => {
+    mount();
+    open();
     [
       'user-options-tooltip',
       'user-options-pdf',
@@ -38,9 +51,150 @@ describe('<UserOptions />', () => {
       'user-options-table',
       'user-options-label',
       'user-options-sort',
-      'user-options-stack'
+      'user-options-stack',
+      'user-options-sort',
+      'user-options-fullscreen',
+      'user-options-anim',
+      'user-options-annotator'
     ].forEach(btn => {
-      cy.get(`[data-cy="${btn}"]`).should('exist').and('be.visible')
+      cy.get(`[data-cy="${btn}"]`).should('exist').and('be.visible');
+    })
+  })
+
+  it('emits', () => {
+    cy.mount(UserOptions, {
+      props: {
+        hasTooltip: true,
+        hasImg: true,
+        hasTable: true,
+        hasSort: true,
+        hasLabel: true,
+        hasStack: true,
+        hasSort: true,
+        hasAnimation: true,
+        hasAnnotator: true,
+        hasFullscreen: true,
+        chartElement: {
+          requestFullscreen: () => {},
+          exitFullscreen: () => {}
+        }
+      }
+    }).then(({ wrapper }) => {
+      open();
+
+      cy.get('[data-cy="user-options-tooltip"]').click().then(() => {
+        expect(wrapper.emitted('toggleTooltip')).to.exist
+      })
+      cy.get('[data-cy="user-options-pdf"]').click().then(() => {
+        expect(wrapper.emitted('generatePdf')).to.exist
+      })
+      cy.get('[data-cy="user-options-xls"]').click().then(() => {
+        expect(wrapper.emitted('generateCsv')).to.exist
+      })
+      cy.get('[data-cy="user-options-img"]').click().then(() => {
+        expect(wrapper.emitted('generateImage')).to.exist
+      })
+      cy.get('[data-cy="user-options-table"]').click().then(() => {
+        expect(wrapper.emitted('toggleTable')).to.exist
+      })
+      cy.get('[data-cy="user-options-label"]').click().then(() => {
+        expect(wrapper.emitted('toggleLabels')).to.exist
+      })
+      cy.get('[data-cy="user-options-sort"]').click().then(() => {
+        expect(wrapper.emitted('toggleSort')).to.exist
+      })
+      cy.get('[data-cy="user-options-stack"]').click().then(() => {
+        expect(wrapper.emitted('toggleStack')).to.exist
+      })
+      cy.get('[data-cy="user-options-fullscreen"]').click().then(() => {
+        expect(wrapper.emitted('toggleFullscreen')).to.exist
+        expect(wrapper.emitted('toggleFullscreen')[0]).to.deep.equal([true])
+      })
+      cy.get('[data-cy="user-options-anim"]').click().then(() => {
+        expect(wrapper.emitted('toggleAnimation')).to.exist
+      })
+      cy.get('[data-cy="user-options-annotator"]').click().then(() => {
+        expect(wrapper.emitted('toggleAnnotator')).to.exist
+      })
+    })
+  })
+
+  it('displays labels in tooltips', () => {
+    cy.viewport(601,500)
+    cy.mount(UserOptions, {
+      props: {
+        hasTooltip: true,
+        hasImg: true,
+        hasTable: true,
+        hasSort: true,
+        hasLabel: true,
+        hasStack: true,
+        hasSort: true,
+        hasAnimation: true,
+        hasAnnotator: true,
+        hasFullscreen: true,
+        titles: {
+          open: 'open',
+          close: 'close',
+          tooltip: 'tooltip',
+          pdf: 'pdf',
+          csv: 'csv',
+          img: 'img',
+          table: 'table',
+          labels: 'labels',
+          sort: 'sort',
+          stack: 'stack',
+          fullscreen: 'fullscreen',
+          animation: 'animation',
+          annotator: 'annotator'
+        }
+      }
+    }).then(() => {
+      open()
+      cy.get('[data-cy="user-options-tooltip"]').trigger('mouseenter').then(() => {
+        cy.get('[data-cy="uo-tooltip"]').should('be.visible').and('contain', 'tooltip')
+        cy.get('[data-cy="user-options-tooltip"]').trigger('mouseout')
+      })
+      cy.get('[data-cy="user-options-pdf"]').trigger('mouseenter').then(() => {
+        cy.get('[data-cy="uo-tooltip"]').should('be.visible').and('contain', 'pdf')
+        cy.get('[data-cy="user-options-pdf"]').trigger('mouseout')
+      })
+      cy.get('[data-cy="user-options-xls"]').trigger('mouseenter').then(() => {
+        cy.get('[data-cy="uo-tooltip"]').should('be.visible').and('contain', 'csv')
+        cy.get('[data-cy="user-options-xls"]').trigger('mouseout')
+      })
+      cy.get('[data-cy="user-options-img"]').trigger('mouseenter').then(() => {
+        cy.get('[data-cy="uo-tooltip"]').should('be.visible').and('contain', 'img')
+        cy.get('[data-cy="user-options-img"]').trigger('mouseout')
+      })
+      cy.get('[data-cy="user-options-table"]').trigger('mouseenter').then(() => {
+        cy.get('[data-cy="uo-tooltip"]').should('be.visible').and('contain', 'table')
+        cy.get('[data-cy="user-options-table"]').trigger('mouseout')
+      })
+      cy.get('[data-cy="user-options-label"]').trigger('mouseenter').then(() => {
+        cy.get('[data-cy="uo-tooltip"]').should('be.visible').and('contain', 'labels')
+        cy.get('[data-cy="user-options-label"]').trigger('mouseout')
+      })
+      cy.get('[data-cy="user-options-sort"]').trigger('mouseenter').then(() => {
+        cy.get('[data-cy="uo-tooltip"]').should('be.visible').and('contain', 'sort')
+        cy.get('[data-cy="user-options-sort"]').trigger('mouseout')
+      })
+      cy.get('[data-cy="user-options-stack"]').trigger('mouseenter').then(() => {
+        cy.get('[data-cy="uo-tooltip"]').should('be.visible').and('contain', 'stack')
+        cy.get('[data-cy="user-options-stack"]').trigger('mouseout')
+      })
+      cy.get('[data-cy="user-options-fullscreen"]').trigger('mouseenter').then(() => {
+        cy.get('[data-cy="uo-tooltip"]').should('be.visible').and('contain', 'fullscreen')
+        cy.get('[data-cy="user-options-fullscreen"]').trigger('mouseout')
+      })
+      cy.get('[data-cy="user-options-anim"]').trigger('mouseenter').then(() => {
+        cy.get('[data-cy="uo-tooltip"]').should('be.visible').and('contain', 'animation')
+        cy.get('[data-cy="user-options-anim"]').trigger('mouseout')
+      })
+      cy.get('[data-cy="user-options-annotator"]').trigger('mouseenter').then(() => {
+        cy.get('[data-cy="uo-tooltip"]').should('be.visible').and('contain', 'annotator')
+        cy.get('[data-cy="user-options-annotator"]').trigger('mouseout')
+      })
     })
   })
 })
--- src/atoms/UserOptions.vue
+++ src/atoms/UserOptions.vue
@@ -281,7 +281,7 @@ const isInfo = ref({
                     <BaseIcon v-if="isItTooltip" name="tooltip" :stroke="color" style="pointer-events: none;"/>
                     <BaseIcon v-else name="tooltipDisabled" :stroke="color" style="pointer-events: none"/>
                 </template>
-                <div dir="auto" v-if="isDesktop && titles.tooltip && !$slots.optionTooltip" :class="{'button-info-left': position === 'left', 'button-info-right' : position === 'right', 'button-info-left-visible': position === 'left' && isInfo.tooltip, 'button-info-right-visible': position === 'right' && isInfo.tooltip }" :style="{ background: backgroundColor, color: color }">
+                <div data-cy="uo-tooltip" dir="auto" v-if="isDesktop && titles.tooltip && !$slots.optionTooltip" :class="{'button-info-left': position === 'left', 'button-info-right' : position === 'right', 'button-info-left-visible': position === 'left' && isInfo.tooltip, 'button-info-right-visible': position === 'right' && isInfo.tooltip }" :style="{ background: backgroundColor, color: color }">
                     {{ titles.tooltip }}
                 </div>
             </button>
@@ -294,7 +294,7 @@ const isInfo = ref({
                     <BaseIcon v-if="isPrinting" name="spin" isSpin :stroke="color" style="pointer-events: none;" />
                     <BaseIcon v-else name="pdf" :stroke="color" style="pointer-events: none;" />
                 </template>
-                <div dir="auto" v-if="isDesktop && titles.pdf && !$slots.optionPdf" :class="{'button-info-left': position === 'left', 'button-info-right' : position === 'right', 'button-info-right-visible': position === 'right' && isInfo.pdf, 'button-info-left-visible': position === 'left' && isInfo.pdf }" :style="{ background: backgroundColor, color: color }">
+                <div data-cy="uo-tooltip" dir="auto" v-if="isDesktop && titles.pdf && !$slots.optionPdf" :class="{'button-info-left': position === 'left', 'button-info-right' : position === 'right', 'button-info-right-visible': position === 'right' && isInfo.pdf, 'button-info-left-visible': position === 'left' && isInfo.pdf }" :style="{ background: backgroundColor, color: color }">
                     {{ titles.pdf }}
                 </div>
             </button>
@@ -306,7 +306,7 @@ const isInfo = ref({
                 <template v-else>
                     <BaseIcon name="excel" :stroke="color" style="pointer-events: none"/>
                 </template>
-                <div dir="auto" v-if="isDesktop && titles.csv && !$slots.optionCsv" :class="{'button-info-left': position === 'left', 'button-info-right' : position === 'right', 'button-info-right-visible': position === 'right' && isInfo.csv, 'button-info-left-visible': position === 'left' && isInfo.csv }" :style="{ background: backgroundColor, color: color }">
+                <div data-cy="uo-tooltip" dir="auto" v-if="isDesktop && titles.csv && !$slots.optionCsv" :class="{'button-info-left': position === 'left', 'button-info-right' : position === 'right', 'button-info-right-visible': position === 'right' && isInfo.csv, 'button-info-left-visible': position === 'left' && isInfo.csv }" :style="{ background: backgroundColor, color: color }">
                     {{ titles.csv }}
                 </div>
             </button>
@@ -319,7 +319,7 @@ const isInfo = ref({
                     <BaseIcon v-if="isImaging" name="spin" isSpin :stroke="color" style="pointer-events: none;" />
                     <BaseIcon v-else name="image" :stroke="color" style="pointer-events: none;" />
                 </template>
-                <div dir="auto" v-if="isDesktop && titles.img && !$slots.optionImg" :class="{'button-info-left': position === 'left', 'button-info-right' : position === 'right', 'button-info-right-visible': position === 'right' && isInfo.img, 'button-info-left-visible': position === 'left' && isInfo.img }" :style="{ background: backgroundColor, color: color }">
+                <div data-cy="uo-tooltip" dir="auto" v-if="isDesktop && titles.img && !$slots.optionImg" :class="{'button-info-left': position === 'left', 'button-info-right' : position === 'right', 'button-info-right-visible': position === 'right' && isInfo.img, 'button-info-left-visible': position === 'left' && isInfo.img }" :style="{ background: backgroundColor, color: color }">
                     {{ titles.img }}
                 </div>
             </button>
@@ -331,7 +331,7 @@ const isInfo = ref({
                 <template v-else>
                     <BaseIcon :name="isTableOpen ? 'tableClose' : 'tableOpen'" :stroke="color" style="pointer-events: none;" />
                 </template>
-                <div dir="auto" v-if="isDesktop && titles.table && !$slots.optionTable" :class="{'button-info-left': position === 'left', 'button-info-right' : position === 'right', 'button-info-right-visible': position === 'right' && isInfo.table, 'button-info-left-visible': position === 'left' && isInfo.table }" :style="{ background: backgroundColor, color: color }">
+                <div data-cy="uo-tooltip" dir="auto" v-if="isDesktop && titles.table && !$slots.optionTable" :class="{'button-info-left': position === 'left', 'button-info-right' : position === 'right', 'button-info-right-visible': position === 'right' && isInfo.table, 'button-info-left-visible': position === 'left' && isInfo.table }" :style="{ background: backgroundColor, color: color }">
                     {{ titles.table }}
                 </div>
             </button>
@@ -343,7 +343,7 @@ const isInfo = ref({
                 <template v-else>
                     <BaseIcon :name="isLabel ? 'labelClose' : 'labelOpen'" :stroke="color" style="pointer-events: none;"/>
                 </template>
-                <div dir="auto" v-if="isDesktop && titles.labels && !$slots.optionLabels" :class="{'button-info-left': position === 'left', 'button-info-right' : position === 'right', 'button-info-right-visible': position === 'right' && isInfo.labels, 'button-info-left-visible': position === 'left' && isInfo.labels }" :style="{ background: backgroundColor, color: color }">
+                <div data-cy="uo-tooltip" dir="auto" v-if="isDesktop && titles.labels && !$slots.optionLabels" :class="{'button-info-left': position === 'left', 'button-info-right' : position === 'right', 'button-info-right-visible': position === 'right' && isInfo.labels, 'button-info-left-visible': position === 'left' && isInfo.labels }" :style="{ background: backgroundColor, color: color }">
                     {{ titles.labels }}
                 </div>
             </button>
@@ -355,7 +355,7 @@ const isInfo = ref({
                 <template v-else>
                     <BaseIcon name="sort" :stroke="color" style="pointer-events: none;"/>
                 </template>
-                <div dir="auto" v-if="isDesktop && titles.sort && !$slots.optionSort" :class="{'button-info-left': position === 'left', 'button-info-right' : position === 'right', 'button-info-right-visible': position === 'right' && isInfo.sort, 'button-info-left-visible': position === 'left' && isInfo.sort }" :style="{ background: backgroundColor, color: color }">
+                <div data-cy="uo-tooltip" dir="auto" v-if="isDesktop && titles.sort && !$slots.optionSort" :class="{'button-info-left': position === 'left', 'button-info-right' : position === 'right', 'button-info-right-visible': position === 'right' && isInfo.sort, 'button-info-left-visible': position === 'left' && isInfo.sort }" :style="{ background: backgroundColor, color: color }">
                     {{ titles.sort }}
                 </div>
             </button>
@@ -368,12 +368,12 @@ const isInfo = ref({
                     <BaseIcon v-if="isItStacked" name="unstack" :stroke="color" style="pointer-events: none;"/>
                     <BaseIcon v-else name="stack" :stroke="color" style="pointer-events: none;"/>
                 </template>
-                <div dir="auto" v-if="isDesktop && titles.stack && !$slots.optionStack" :class="{'button-info-left': position === 'left', 'button-info-right' : position === 'right', 'button-info-right-visible': position === 'right' && isInfo.stack, 'button-info-left-visible': position === 'left' && isInfo.stack }" :style="{ background: backgroundColor, color: color }">
+                <div data-cy="uo-tooltip" dir="auto" v-if="isDesktop && titles.stack && !$slots.optionStack" :class="{'button-info-left': position === 'left', 'button-info-right' : position === 'right', 'button-info-right-visible': position === 'right' && isInfo.stack, 'button-info-left-visible': position === 'left' && isInfo.stack }" :style="{ background: backgroundColor, color: color }">
                     {{ titles.stack }}
                 </div>
             </button>
 
-            <button tabindex="0" v-if="hasFullscreen" data-cy="user-options-sort" class="vue-ui-user-options-button" @mouseenter="isInfo.fullscreen = true" @mouseout="isInfo.fullscreen = false" @click="toggleFullscreen(isFullscreen ? 'out' : 'in')">
+            <button tabindex="0" v-if="hasFullscreen" data-cy="user-options-fullscreen" class="vue-ui-user-options-button" @mouseenter="isInfo.fullscreen = true" @mouseout="isInfo.fullscreen = false" @click="toggleFullscreen(isFullscreen ? 'out' : 'in')">
                 <template v-if="$slots.optionFullscreen">
                     <slot name="optionFullscreen" v-bind="{ toggleFullscreen, isFullscreen }"/>
                 </template>
@@ -381,7 +381,7 @@ const isInfo = ref({
                     <BaseIcon v-if="isFullscreen" name="exitFullscreen" :stroke="color" style="pointer-events: none;"/>
                     <BaseIcon v-if="!isFullscreen" name="fullscreen" :stroke="color" style="pointer-events: none;"/>
                 </template>
-                <div dir="auto" v-if="isDesktop && titles.fullscreen && !$slots.optionFullscreen" :class="{'button-info-left': position === 'left', 'button-info-right' : position === 'right', 'button-info-right-visible': position === 'right' && isInfo.fullscreen, 'button-info-left-visible': position === 'left' && isInfo.fullscreen }" :style="{ background: backgroundColor, color: color }">
+                <div data-cy="uo-tooltip" dir="auto" v-if="isDesktop && titles.fullscreen && !$slots.optionFullscreen" :class="{'button-info-left': position === 'left', 'button-info-right' : position === 'right', 'button-info-right-visible': position === 'right' && isInfo.fullscreen, 'button-info-left-visible': position === 'left' && isInfo.fullscreen }" :style="{ background: backgroundColor, color: color }">
                     {{ titles.fullscreen }}
                 </div>
             </button>
@@ -394,12 +394,12 @@ const isInfo = ref({
                     <BaseIcon v-if="isAnimated" name="play" :stroke="color" style="pointer-events: none;"/>
                     <BaseIcon v-if="!isAnimated" name="pause" :stroke="color" style="pointer-events: none;"/>
                 </template>
-                <div dir="auto" v-if="isDesktop && titles.fullscreen && !$slots.optionAnimation" :class="{'button-info-left': position === 'left', 'button-info-right' : position === 'right', 'button-info-right-visible': position === 'right' && isInfo.animation, 'button-info-left-visible': position === 'left' && isInfo.animation }" :style="{ background: backgroundColor, color: color }">
+                <div data-cy="uo-tooltip" dir="auto" v-if="isDesktop && titles.fullscreen && !$slots.optionAnimation" :class="{'button-info-left': position === 'left', 'button-info-right' : position === 'right', 'button-info-right-visible': position === 'right' && isInfo.animation, 'button-info-left-visible': position === 'left' && isInfo.animation }" :style="{ background: backgroundColor, color: color }">
                     {{ titles.animation }}
                 </div>
             </button>
 
-            <button tabindex="0" v-if="hasAnnotator" data-cy="user-options-anim" class="vue-ui-user-options-button" @mouseenter="isInfo.annotator = true" @mouseout="isInfo.annotator = false" @click="toggleAnnotator">
+            <button tabindex="0" v-if="hasAnnotator" data-cy="user-options-annotator" class="vue-ui-user-options-button" @mouseenter="isInfo.annotator = true" @mouseout="isInfo.annotator = false" @click="toggleAnnotator">
                 <template v-if="$slots.optionAnnotator">
                     <slot name="optionAnnotator" v-bind="{ toggleAnnotator, isAnnotator }"/>
                 </template>
@@ -407,7 +407,7 @@ const isInfo = ref({
                     <BaseIcon v-if="isAnnotator" name="annotatorDisabled" :stroke="color" style="pointer-events: none;"/>
                     <BaseIcon v-if="!isAnnotator" name="annotator" :stroke="color" style="pointer-events: none;"/>
                 </template>
-                <div dir="auto" v-if="isDesktop && titles.annotator && !$slots.optionAnnotator" :class="{'button-info-left': position === 'left', 'button-info-right' : position === 'right', 'button-info-right-visible': position === 'right' && isInfo.annotator, 'button-info-left-visible': position === 'left' && isInfo.annotator }" :style="{ background: backgroundColor, color: color }">
+                <div data-cy="uo-tooltip" dir="auto" v-if="isDesktop && titles.annotator && !$slots.optionAnnotator" :class="{'button-info-left': position === 'left', 'button-info-right' : position === 'right', 'button-info-right-visible': position === 'right' && isInfo.annotator, 'button-info-left-visible': position === 'left' && isInfo.annotator }" :style="{ background: backgroundColor, color: color }">
                     {{ titles.annotator }}
                 </div>
             </button>
