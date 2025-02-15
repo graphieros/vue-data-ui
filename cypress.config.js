7223e9f0c7da4816c650388c5487617a8be39d13
graphieros <alec.lloyd.probert@gmail.com>
2025-02-15 10:46:30 +0100

    Add dedicated vite config for cy

--- cypress.config.js
+++ cypress.config.js
@@ -1,18 +1,19 @@
 import { defineConfig } from "cypress";
-
 import fs from "fs";
+import viteConfig from "./vite.config.cypress.js";
 
 export default defineConfig({
   component: {
     devServer: {
       framework: "vue",
       bundler: "vite",
+      viteConfig
     },
     setupNodeEvents(on, config) {
       on('task', {
         clearDownloads() {
           const downloadsFolder = 'cypress/downloads';
-    
+
           try {
             fs.readdirSync(downloadsFolder).forEach((file) => {
               const filePath = `${downloadsFolder}/${file}`;
---
+++ vite.config.cypress.js
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+
+export default defineConfig({
+    plugins: [vue()],
+    resolve: {
+        alias: {
+            'vue': 'vue/dist/vue.esm-bundler.js'
+        }
+    }
+});
