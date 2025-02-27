34a3ac91d4720dd0ea626e12711797a727775c02
graphieros <alec.lloyd.probert@gmail.com>
2025-02-27 06:44:47 +0100

    Tests - Improve VueUiGalaxy component test

--- src/components/vue-ui-galaxy.cy.js
+++ src/components/vue-ui-galaxy.cy.js
@@ -32,4 +32,35 @@ describe('<VueUiGalaxy />', () => {
             cy.get('[data-cy="datapoint-path"]').should('exist').and('be.visible').and('have.length', dataset.length);
         });
     });
+
+    it('emits', () => {
+        cy.mount(VueUiGalaxy, {
+            props: {
+                config,
+                dataset
+            }
+        }).then(({ wrapper }) => {
+            cy.log('@selectLegend');
+            cy.get('[data-cy="legend-item-0"]').click({ force: true }).then(() => {
+                expect(wrapper.emitted('selectLegend')).to.exist;
+            })
+
+            cy.log('@selectDatapoint');
+            cy.get('[data-cy="tooltip-trap"]').first().click({ force: true }).then(() => {
+                expect(wrapper.emitted('selectDatapoint')).to.exist;
+                expect(wrapper.emitted('selectDatapoint')[0][0]).to.have.keys(
+                    'absoluteValues',
+                    'color',
+                    'id',
+                    'name',
+                    'path',
+                    'points',
+                    'proportion',
+                    'seriesIndex',
+                    'value'
+                );
+                expect(wrapper.emitted('selectDatapoint')[0][0].seriesIndex).to.equal(1);
+            });
+        });
+    });
 });
