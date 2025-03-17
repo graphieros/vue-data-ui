2b2a789245b8a0ae163689a475399ec5cd66d397
graphieros <alec.lloyd.probert@gmail.com>
2025-03-17 07:36:32 +0100

    Atoms - UserOptions - Add menuIcon slot

--- src/atoms/UserOptions.vue
+++ src/atoms/UserOptions.vue
@@ -269,7 +269,9 @@ const isInfo = ref({
         :style="`z-index: ${zIndex}; height: 34px; position: ${isFullscreen ? 'fixed' : 'absolute'}; top: 0; ${position === 'right' ? `right:${isFullscreen ? '12px': '0'}` : `left:${isFullscreen ? '12px' : '0'}`}; padding: 4px; background:transparent;`">
 
         <div tabindex="0" :title="isOpen ? titles.close || '' : titles.open || ''" data-cy="user-options-summary" :style="`width:32px; position: absolute; top: 0;${position === 'right' ? `right: ${offsetX ? offsetX : noOffset ? 0 : 4}px` : `left: ${noOffset ? 0 : 4}px`}; padding: 0 0px; display: flex; align-items:center;justify-content:center;height: 36px;  cursor:pointer; background:transparent`" @click.stop="toggle" @keypress.enter="toggle">
-            <BaseIcon  :name="isOpen ? 'close' : 'menu'" :stroke="color" :stroke-width="2" />
+            <slot name="menuIcon" v-bind="{ isOpen, color }">
+                <BaseIcon :name="isOpen ? 'close' : 'menu'" :stroke="color" :stroke-width="2" />
+            </slot>
         </div>
         <div :data-open="isOpen" data-cy="user-options-drawer" :class="{'vue-ui-user-options-drawer': true}" :style="`background:${backgroundColor}; ${position === 'right' ? `right: ${offsetX ? offsetX : noOffset ? 0 : 4}px` : `left: ${noOffset ? 0 : 4}px`}`">
 
