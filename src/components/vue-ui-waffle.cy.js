3cf7831482dee4aa1eb1806b48a8d082d6dcb045
graphieros <alec.lloyd.probert@gmail.com>
2026-01-24 00:45:24 +0100

    Fix - Test - Fix issues with observer

--- src/components/vue-ui-waffle.cy.js
+++ src/components/vue-ui-waffle.cy.js
@@ -6,16 +6,17 @@ const { config, dataset } = components.find(c => c.name === 'VueUiWaffle');
 
 describe('<VueUiWaffle />', () => {
 	beforeEach(() => {
-		cy.viewport(500,600);
-	})
+		cy.viewport(500, 600);
+	});
+
 	it('renders', () => {
 		cy.mount(VueUiWaffle, {
 			props: {
 				dataset,
-				config
+				// Disable responsive to avoid ResizeObserver loop error in Cypress runner
+				config: { ...config, responsive: false }
 			}
 		}).then(() => {
-			
 			testCommonFeatures({
 				userOptions: true,
 				title: true,
@@ -28,13 +29,26 @@ describe('<VueUiWaffle />', () => {
 			});
 
 			cy.log('datapoints');
-			cy.get('[data-cy="datapoint-underlayer"]').should('exist').and('be.visible').and('have.length', config.style.chart.layout.grid.size ** 2);
-			cy.get('[data-cy="datapoint-rect"]').should('exist').and('be.visible').and('have.length', config.style.chart.layout.grid.size ** 2);
-			cy.get('[data-cy="datapoint-caption"]').as('captions').should('exist').and('be.visible').and('have.length', dataset.length);
+			cy.get('[data-cy="datapoint-underlayer"]')
+				.should('exist')
+				.and('be.visible')
+				.and('have.length', config.style.chart.layout.grid.size ** 2);
+
+			cy.get('[data-cy="datapoint-rect"]')
+				.should('exist')
+				.and('be.visible')
+				.and('have.length', config.style.chart.layout.grid.size ** 2);
+
+			cy.get('[data-cy="datapoint-caption"]')
+				.as('captions')
+				.should('exist')
+				.and('be.visible')
+				.and('have.length', dataset.length);
+
 			cy.get('@captions').each((caption, i) => {
 				cy.wrap(caption)
 					.should('contain', dataset[i].name)
-					.and('contain', dataset[i].values.reduce((a, b) => a + b, 0))
+					.and('contain', dataset[i].values.reduce((a, b) => a + b, 0));
 			});
 		});
 	});
@@ -43,15 +57,21 @@ describe('<VueUiWaffle />', () => {
 		cy.mount(VueUiWaffle, {
 			props: {
 				dataset,
-				config
+				config: { ...config, responsive: false }
 			}
 		}).then(({ wrapper }) => {
 			cy.spy(window, 'requestAnimationFrame').as('rafSpy');
 
 			cy.log('@selectLegend');
-			cy.get('[data-cy="legend-item"]').first().click({ force: true }).then(() => {
+
+			cy.get('[data-cy="legend-item"]')
+				.first()
+				.click({ force: true });
+
+			cy.get('@rafSpy').should('have.been.called');
+
+			cy.wrap(null, { timeout: 10000 }).should(() => {
 				expect(wrapper.emitted('selectLegend')).to.exist;
-				cy.get('@rafSpy').should('have.been.called');
 			});
 		});
 	});
