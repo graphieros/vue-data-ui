fec09e6d554ce0fa5c45cc79077e17d04c3a045d
graphieros <alec.lloyd.probert@gmail.com>
2026-01-24 00:32:59 +0100

    Improvement - Add BaseLegendToggle atom component

---
+++ src/atoms/BaseLegendToggle.vue
@@ -0,0 +1,45 @@
+<script setup>
+import BaseIcon from "./BaseIcon.vue";
+
+const props = defineProps({
+    backgroundColor: { type: String, default: '#CCCCCC' },
+    color: { type: String, default: '#2D353C' },
+    fontSize: { type: Number, default: 14 },
+    checked: { type: Boolean },
+});
+
+const emit = defineEmits(['toggle']);
+
+</script>
+
+<template>
+    <div class="vue-ui-legend-toggle-wrapper" data-dom-to-png-ignore>
+        <div
+            class="vue-ui-legend-toggle"
+            role="button" 
+            tabindex="0"
+            @click="emit('toggle')"
+            @keydown.enter="emit('toggle')"
+            :style="{
+                position: 'relative',
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+                backgroundColor,
+                padding: fontSize / 4 + 'px'
+            }">
+            <BaseIcon 
+                :name="checked ? 'minus' : 'check'" 
+                :stroke="color"
+                :size="fontSize * 0.6"
+                :stroke-width="fontSize / 4"
+            />
+        </div>
+    </div>
+</template>
+
+<style scoped>
+    .vue-ui-legend-toggle-wrapper {
+        cursor: pointer; 
+    }
+</style>
