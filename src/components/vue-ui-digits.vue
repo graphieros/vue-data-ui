791134e7f4e80b6492e6f9b3454eb652458a48a5
graphieros <alec.lloyd.probert@gmail.com>
2026-03-19 07:34:06 +0100

    fix(VueUiDigits): improve a11y

--- src/components/vue-ui-digits.vue
+++ src/components/vue-ui-digits.vue
@@ -2,8 +2,9 @@
 import { computed } from "vue";
 import Digit from '../atoms/Digit.vue';
 import { useNestedProp } from "../useNestedProp";
-import { XMLNS } from "../lib";
+import { createUid, XMLNS } from "../lib";
 import { useConfig } from "../useConfig";
+import PackageVersion from "../atoms/PackageVersion.vue";
 
 const { vue_ui_digits: DEFAULT_CONFIG } = useConfig();
 
@@ -55,18 +56,47 @@ const maxY = computed(() => {
     return Math.max(...digits.value.map(d => d.x)) + 36
 })
 
+const uid = createUid();
+
 </script>
 
 <template>
-    <svg class="vue-data-ui-component vue-ui-digits" :xmlns="XMLNS" :viewBox="`0 0 ${maxY} 80`" :style="`background:${FINAL_CONFIG.backgroundColor};${FINAL_CONFIG.height ? `height:${FINAL_CONFIG.height};` : ''}${FINAL_CONFIG.width ? `width:${FINAL_CONFIG.width}` : ''}`">
-        <Digit
-            v-for="digit in digits"
-            :x="digit.x"
-            :y="digit.y"
-            :quanta="digit.quanta"
-            :color="FINAL_CONFIG.digits.color"
-            :backgroundColor="FINAL_CONFIG.digits.skeletonColor"
-            :thickness="FINAL_CONFIG.digits.thickness"
-        />
-    </svg>
+    <div>
+        <div class="sr-only" :id="`digit-${uid}`">
+            {{ dataset }}
+        </div>
+        <svg 
+            class="vue-data-ui-component vue-ui-digits" 
+            :xmlns="XMLNS" 
+            :viewBox="`0 0 ${maxY} 80`" 
+            :style="`background:${FINAL_CONFIG.backgroundColor};${FINAL_CONFIG.height ? `height:${FINAL_CONFIG.height};` : ''}${FINAL_CONFIG.width ? `width:${FINAL_CONFIG.width}` : ''}`"
+            :aria-describedby="`digit-${uid}`"
+        >
+            <PackageVersion />
+            <Digit
+                v-for="digit in digits"
+                :x="digit.x"
+                :y="digit.y"
+                :quanta="digit.quanta"
+                :color="FINAL_CONFIG.digits.color"
+                :backgroundColor="FINAL_CONFIG.digits.skeletonColor"
+                :thickness="FINAL_CONFIG.digits.thickness"
+            />
+        </svg>
+    </div>
 </template>
+
+<style scoped>
+.sr-only {
+    position: absolute;
+    width: 1px;
+    height: 1px;
+    padding: 0;
+    margin: -1px;
+    overflow: hidden;
+    clip-path: inset(50%);
+    clip: rect(0 0 0 0);
+    white-space: normal;
+    border: 0;
+}
+</style>
